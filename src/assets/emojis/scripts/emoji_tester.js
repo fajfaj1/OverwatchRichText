f1e933ab788263c5ece86e965aefc1eb1b226563
fajfaj <fajfajdev@gmail.com>
2025-07-27 01:43:57 +0200

    optimized strategy in emoji_tester.js

--- src/assets/emojis/scripts/emoji_tester.js
+++ src/assets/emojis/scripts/emoji_tester.js
@@ -4,15 +4,39 @@ import ChildProcess from 'child_process';
 import readline from 'readline';
 import fs from 'fs';
 
+const startTimestamp = Date.now();
+
 const START = 0;
-const END = 100;
+const END = 11160;
 
 const BATCH_SIZE = 10;
 const slicedIDList = ids.slice(START, END);
 
+let confirmed_ids = [];
+
 console.log(`Running emoji check on emojis from ${START} to ${END - 1}`);
 
-let totalBatchCount = Math.ceil(slicedIDList.length / BATCH_SIZE);
+let totalIdCount = slicedIDList.length;
+
+function humanizeMS(ms) {
+    // const timeInSeconds = ms / 1000;
+    const seconds = Math.round((ms / 1000) % 60);
+    // const timeInMinutes = (timeInSeconds - seconds) / 60;
+    const minutes = Math.round((ms / (60 * 1000)) % 60);
+    // const timeInHours = (timeInMinutes - seconds) / 60;
+    const hours = Math.round((ms / (60 * 60 * 1000)) % 24);
+    let time = '';
+    if (hours > 0) {
+        time += `${hours}h`;
+    }
+    if (minutes > 0) {
+        time += ` ${hours}m`;
+    }
+    if (seconds > 0 || hours === 0) {
+        time += ` ${seconds}s`;
+    }
+    return time;
+}
 
 function saveToClipboard(text) {
     ChildProcess.spawn('clip').stdin.end(text);
@@ -30,78 +54,92 @@ function decodeHumanInput(text) {
     const texts = text.replaceAll(' ', '').split(',');
 
     const indices = [];
-    texts
-        .map((text) => {
-            if (indexExpression.test(text)) {
-                indices.push(text);
-            } else if (rangeExpression.test(text)) {
-                const boundaries = text.split('-').slice(0, 2);
-                const endIndex = boundaries.pop();
-                const startIndex = boundaries.pop();
+    texts.map((text) => {
+        if (indexExpression.test(text)) {
+            indices.push(text);
+        } else if (rangeExpression.test(text)) {
+            const boundaries = text.split('-').slice(0, 2);
+            const endIndex = boundaries.pop();
+            const startIndex = boundaries.pop();
 
-                for (let i = startIndex; i <= endIndex; i++) {
-                    indices.push(`${i}`);
-                }
-            } else {
-                throw new Error(`Failed to parse item: "${text}"`);
+            for (let i = startIndex; i <= endIndex; i++) {
+                indices.push(`${i}`);
             }
-        })
-        .map((index) => +index);
-    return indices;
+        } else {
+            throw new Error(`Failed to parse item: "${text}"`);
+        }
+    });
+    return indices.map((index) => +index);
 }
+let checkedIds = 0;
 
 // Batching strategy: try with 10 elements, if 4 or more are valid, try ones after the matched ones
-async function askForIds(idList, batchNumber) {
+async function askForIdsFrom(startPosition) {
+    const idList = slicedIDList.slice(
+        startPosition,
+        startPosition + BATCH_SIZE
+    );
+
+    if (idList.length === 0) {
+        return;
+    }
+
     const message = idList.map((id, index) => `${index}<TXC00${id}>`).join('');
 
     saveToClipboard(message);
-    console.log('\n' + idList.map((id, index) => `${index}. ${id}`).join('\n'));
+    console.log(
+        '\n' +
+            idList
+                .map((id, index) => `${index}. ${id}  <TXC00${id}>`)
+                .join('\n')
+    );
 
     await new Promise((resolve) => {
         const rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout,
         });
+        const completion =
+            Math.round((checkedIds / totalIdCount) * 10000) / 100;
+
+        const timeRemainingMS =
+            ((Date.now() - startTimestamp) / completion) * 100;
         rl.question(
-            `[${batchNumber}/${totalBatchCount}] Which ids are correct? `,
+            `[${completion}%] (ETA: ${humanizeMS(
+                timeRemainingMS
+            )}) Which ids are correct? `,
             async (answer) => {
                 let abort = false;
-                let valid_ids = [];
+                let validIds = [];
+                let indices = [];
                 try {
-                    valid_ids.push(
-                        ...decodeHumanInput(answer).map(
-                            (index) => idList[index]
-                        )
-                    );
+                    indices.push(...decodeHumanInput(answer));
+                    validIds.push(...indices.map((index) => idList[index]));
                 } catch (err) {
                     console.error(err);
                     console.log(`Please try again.`);
                     rl.close();
-                    await askForIds(idList, batchNumber);
+                    await askForIdsFrom(startPosition);
                     abort = true;
                 }
 
                 if (!abort) {
-                    confirmed_ids = [...confirmed_ids, ...valid_ids];
-                    if (valid_ids.length >= 4) {
-                        const newBatch = [...idList];
+                    confirmed_ids.push(...validIds);
+                    if (validIds.length >= 4) {
                         // Leaves only ids after last valid id
-                        let startRemoving = false;
-                        for (let i = newBatch.length; i >= 0; i--) {
-                            if (valid_ids.includes(newBatch[i])) {
-                                startRemoving = true;
-                            }
-                            if (startRemoving) {
-                                delete newBatch[i];
-                            } else {
-                            }
-                        }
-
-                        rl.close();
-                        await askForIds(
-                            idList.filter((id) => !valid_ids.includes(id)),
-                            batchNumber
+                        const startPositionOffset = Math.max(...indices) + 1;
+                        console.log(
+                            `Four or more elements matched, offsetting the start position by ${startPositionOffset} instead of ${BATCH_SIZE} ${startPositionOffset}`
                         );
+                        rl.close();
+                        checkedIds += startPositionOffset;
+                        await askForIdsFrom(
+                            startPosition + startPositionOffset
+                        );
+                    } else {
+                        rl.close();
+                        checkedIds += 10;
+                        await askForIdsFrom(startPosition + BATCH_SIZE);
                     }
                 }
                 rl.close();
@@ -111,16 +149,9 @@ async function askForIds(idList, batchNumber) {
     });
 }
 
-let confirmed_ids = [];
-for (let i = 0; i < slicedIDList.length; i += BATCH_SIZE) {
-    // console.log(i);
-    const batchIds = [];
-    for (let j = 0; j < BATCH_SIZE && slicedIDList[i + j] !== undefined; j++) {
-        batchIds.push(slicedIDList[i + j]);
-    }
+await askForIdsFrom(0);
 
-    await askForIds(batchIds, i / BATCH_SIZE + 1);
-}
+console.log(`Writing ${confirmed_ids.length} IDs to disk.`);
 
 fs.writeFileSync(
     './src/assets/emojis/scripts/valid_ids.txt',
