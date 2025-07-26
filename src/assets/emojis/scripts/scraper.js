60a30d2e1108b10893840c69bcf681f91a3749aa
fajfaj <fajfajdev@gmail.com>
2025-07-26 12:48:39 +0200

    remove clutter from scraper.js

--- src/assets/emojis/scripts/scraper.js
+++ src/assets/emojis/scripts/scraper.js
@@ -1,11 +1,7 @@
 import fs from 'node:fs';
-import client from 'https';
 import { ids } from './ids.js';
 import sharp from 'sharp';
 import { pipeline } from 'stream/promises';
-import { resolve } from 'node:path';
-import { fail } from 'node:assert';
-const timeoutMs = 5000;
 
 const assetUrl = (id) =>
     `https://assets.overwatchitemtracker.com/textures/${id}.png`;
@@ -48,7 +44,6 @@ async function downscaleAndDownload(url, filepath) {
             `Failed to fetch image from ${url} ${response.status} ${response.statusText}`
         );
     }
-    // console.log(`response received`);
     await pipeline(
         response.body,
         sharp().resize(512, 512, {
@@ -58,7 +53,6 @@ async function downscaleAndDownload(url, filepath) {
         fs.createWriteStream(filepath)
     );
 
-    // console.log(`file saved`);
     successes++;
 }
 
@@ -75,25 +69,3 @@ for (let i = start; i < end; i++) {
     }
     await new Promise((resolve) => setTimeout(() => resolve(), 10));
 }
-
-// // let i = 0;
-// let successes = 0;
-// const start = 700;
-// const end = 3000;
-
-// const idCount = end - start;
-// const slicedIds = ids.slice(start, end);
-// for (let i = 0; i < slicedIds; i++) {
-//     const fileurl = url.replace('%ID%', id);
-//     await downloadImage(fileurl, filepath.replace('%ID%', id))
-//         .then(() => successes++)
-//         .catch((err) => {
-//             console.err(`Failed to download ${id} from ${url}:`);
-//             console.error(err);
-//         });
-//     i++;
-//     if (i % 100 === 0) {
-//         console.log(`Completed ${Math.round((successes / idCount) * 100)}%`);
-//     }
-//     await new Promise((r) => setTimeout(r, 1000));
-// }
